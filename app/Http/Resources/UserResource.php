4f0a0de8843a661e81641ead8adf8b18cfcc5fb7
Urvish-patel810 <patelurvish47262@gmail.com>
2024-03-13 17:33:12 +0530

    user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,15 +14,18 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'user_id' => $this->id,
-            'first_name' => $this->first_name,
-            'email'     => $this->email,
-            'token'     => $this->createToken("Token")->plainTextToken,
-            'roles'     => $this->roles->pluck('name') ?? [],
-            'roles.permissions' => $this->getPermissionsViaRoles()->pluck(['name']) ?? [],
-            'permissions'=> $this->permissions->pluck('name') ?? []
-
+        // Get all fields of the user model
+        $userFields = $this->resource->toArray();
+    
+        // Merge additional fields
+        $additionalFields = [
+            'token' => $this->createToken("Token")->plainTextToken,
+            'roles' => $this->roles->pluck('name') ?? [],
+            'roles_permissions' => $this->getPermissionsViaRoles()->pluck('name') ?? [],
+            'permissions' => $this->permissions->pluck('name') ?? []
         ];
+    
+        // Merge all fields together
+        return array_merge($userFields, $additionalFields);
     }
 }
